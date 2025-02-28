6227a84e97d14592ba628598422ab2f160e74bf5
lbrooks81 <143453730+lbrooks81@users.noreply.github.com>
2025-02-28 15:31:11 -0500

    Images are displaying List page is styled slightly better

--- recipe-book-app/app/Details.tsx
+++ recipe-book-app/app/Details.tsx
@@ -2,8 +2,21 @@ import {View, Text, Button, StyleSheet, Image, ImageSourcePropType} from "react-
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
 import {RootStackParamList} from "@/app/index";
 import {Recipe} from "@/app/List";
+import {string} from "prop-types";
 interface DetailsScreenProps extends NativeStackScreenProps<RootStackParamList, "Details">{}
 
+export const images = {
+    "Spaghetti": require("../assets/images/spaghetti.jpg"),
+    "Salad": require("../assets/images/salad.jpg"),
+    "Chicken Curry": require("../assets/images/chicken_curry.jpg"),
+    "Pancakes": require("../assets/images/pancakes.jpg"),
+    "Grilled Cheese Sandwich": require("../assets/images/grilled_cheese.jpg"),
+    "Chocolate Cake": require("../assets/images/chocolate_cake.jpg"),
+    "Fried Rice": require("../assets/images/fried_rice.jpg"),
+    "Pasta Carbonara": require("../assets/images/pasta_carbonara.jpg"),
+    "Tacos": require("../assets/images/tacos.jpg"),
+    "Apple Pie": require("../assets/images/apple_pie.jpg")
+};
 export default function DetailScreen({route} : DetailsScreenProps)
 {
     const recipe: Recipe = route.params.recipe;
@@ -14,7 +27,33 @@ export default function DetailScreen({route} : DetailsScreenProps)
             <Text>{recipe.description}</Text>
             <Text>{recipe.ingredients}</Text>
             <Text>{recipe.instructions}</Text>
-            <Image source={imageSource}></Image>
+            <Image style={style.image} source={selectImage(recipe.title)}></Image>
         </View>
     )
 }
+export function selectImage(recipeName: string)
+{
+    const imageArray = {
+        'Spaghetti': images.Spaghetti,
+        'Salad': images.Salad,
+        'Chicken Curry': images['Chicken Curry'],
+        'Pancakes': images.Pancakes,
+        'Grilled Cheese Sandwich': images['Grilled Cheese Sandwich'],
+        'Chocolate Cake': images['Chocolate Cake'],
+        'Fried Rice': images['Fried Rice'],
+        'Pasta Carbonara': images['Pasta Carbonara'],
+        'Tacos': images.Tacos,
+        'Apple Pie': images['Apple Pie']
+    }
+    // @ts-ignore
+    return imageArray[recipeName];
+}
+
+const style = StyleSheet.create({
+    image: {
+        resizeMode: "contain",
+        width: "100%",
+        height: "100%",
+    }
+
+});
--- recipe-book-app/app/List.tsx
+++ recipe-book-app/app/List.tsx
@@ -1,7 +1,8 @@
-import {View, Text, Button, StyleSheet} from "react-native";
+import {View, Text, Button, StyleSheet, Image} from "react-native";
 import recipes from "@/assets/recipes.json";
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
 import {RootStackParamList} from "@/app/index";
+import {selectImage} from "@/app/Details";
 interface HomeScreenProps extends NativeStackScreenProps<RootStackParamList, "List"> {}
 
 export class Recipe {
@@ -26,14 +27,43 @@ export default function ListScreen({navigation} : HomeScreenProps) {
     return (
         <View>
             {recipes.map((recipe, index) => (
-                <View key={index}>
-                    <Text>{recipe.title}</Text>
-                    <Text>{recipe.description}</Text>
-                    <Button title="View Recipe" onPress={() =>
-                        {onPressDetails(new Recipe(recipe))}}/>
+                <View style={styles.container} key={index}>
+                    <Image style={styles.thumbnail} source={selectImage(recipe.title)}></Image>
+                    <View style={styles.col}>
+                        <Text>{recipe.title}</Text>
+                        <Text>{recipe.description}</Text>
+                    </View>
+                    <View style={styles.col}>
+                        <Button title="View Recipe" onPress={() =>
+                            {onPressDetails(new Recipe(recipe))}}/>
+                    </View>
                 </View>
             ))}
         </View>
     );
-
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        flexDirection: "row",
+        borderStyle: "solid",
+        borderColor: "black",
+        borderWidth: 1,
+        padding: 10,
+        margin: 10,
+        maxHeight: 200
+    },
+    col: {
+        flexDirection: "column",
+
+    },
+    thumbnail: {
+        width: 50,
+        height: 50,
+        resizeMode: "contain",
+        marginRight: 5
+    }
+})
